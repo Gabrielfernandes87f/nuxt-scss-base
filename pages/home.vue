2e6b80d6d6994d4e4790949e5f9369e60ff084d9
Gabrielfernandes87f <gabrielfernandesfotografias@outlook.com>
2022-04-19 16:38:39 -0300

    update

--- pages/home.vue
+++ pages/home.vue
@@ -1,7 +1,18 @@
 <template>
+<div>
+
   <h1>
       Discord
+      <a href="#" class="buttom linkscss" @click="dark=true"> tema dark</a>
+
   </h1>
+      <button @click="toglleDarck=false">
+        false
+      </button>
+            <button @click="toglleDarck=true">
+        true
+      </button>
+</div>
 </template>
 
 <script>
